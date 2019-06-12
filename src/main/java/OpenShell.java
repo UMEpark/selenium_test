3fc1d31e73cbaaeb85ed2aa82e286526d54f1176
name <email>
2019-06-12 18:00:05 +0900

    4th commit

--- src/main/java/OpenShell.java
+++ src/main/java/OpenShell.java
@@ -1,2 +1,9 @@
+import java.io.IOException;
+
 public class OpenShell {
+    public static void staticNetwork(){
+        String directory_static = "/home/network-18/static_network.sh";
+        ProcessBuilder processBuilder = new ProcessBuilder();
+
+    }
 }

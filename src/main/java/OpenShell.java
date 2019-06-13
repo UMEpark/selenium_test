2b263cdaf7ae79047599a1e06a3d0a5d272523f1
name <email>
2019-06-13 14:04:26 +0900

    5th commit

--- src/main/java/OpenShell.java
+++ src/main/java/OpenShell.java
@@ -1,9 +1,35 @@
 import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class OpenShell {
     public static void staticNetwork(){
-        String directory_static = "/home/network-18/static_network.sh";
-        ProcessBuilder processBuilder = new ProcessBuilder();
+        Process p;
+        try{
+            List<String> cmdList = new ArrayList<>();
+            cmdList.add("sh");
+            cmdList.add("/home/network-18/static_network.sh");
+            ProcessBuilder staticNetConfig = new ProcessBuilder(cmdList);
+            p = staticNetConfig.start();
+            p.waitFor();
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            String line;
+
+            while((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+
+        } catch(IOException e){
+            e.printStackTrace();
+        } catch(InterruptedException e){
+            e.printStackTrace();
+        }
+    }
+
+    public static void dhcpNetwork(){
 
     }
 }

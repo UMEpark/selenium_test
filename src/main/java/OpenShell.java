d305820c678ce9f5e34d7d8e0df56e6d793b353f
name <email>
2019-06-13 14:06:03 +0900

    5th commit

--- src/main/java/OpenShell.java
+++ src/main/java/OpenShell.java
@@ -30,6 +30,26 @@ public class OpenShell {
     }
 
     public static void dhcpNetwork(){
+        Process p;
+        try{
+            List<String> cmdList = new ArrayList<>();
+            cmdList.add("sh");
+            cmdList.add("/home/network-18/DHCP_network.sh");
+            ProcessBuilder dhcpNetConfig = new ProcessBuilder(cmdList);
+            p = dhcpNetConfig.start();
+            p.waitFor();
 
+            BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            String line;
+
+            while((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+
+        } catch(IOException e){
+            e.printStackTrace();
+        } catch(InterruptedException e){
+            e.printStackTrace();
+        }
     }
 }
